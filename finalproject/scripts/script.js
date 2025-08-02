28799cc1abfc9f8ac1f70f43b4ecabc18f710fa7
Hernan Singh! <hernanjoelsingh@gmail.com>
2025-08-02 20:41:24 -0300

    first modifications

---
+++ finalproject/scripts/script.js
@@ -0,0 +1,12 @@
+const menuButton = document.getElementById('menu');
+const nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
