118c05c1d3fa8b1e10572380bf7f0d4f4ff2fed0
Hernan Singh! <hernanjoelsingh@gmail.com>
2025-08-09 15:09:15 -0300

    form

--- finalproject/scripts/tracker.js
+++ finalproject/scripts/tracker.js
@@ -1,12 +1,15 @@
-const menuButton = document.getElementById('menu');
-const navMobile = document.querySelector('.nav-mobile');
+const menuButton = document.getElementById("menu");
+const navMobile = document.querySelector(".nav-mobile");
 
-menuButton.addEventListener('click', function() {
-    menuButton.classList.toggle('open');
-    navMobile.classList.toggle('open');
+menuButton.addEventListener("click", function () {
+  menuButton.classList.toggle("open");
+  navMobile.classList.toggle("open");
 });
 
-document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+document.getElementById(
+  "lastModified"
+).textContent = `Last Modified: ${document.lastModified}`;
 
+document.getElementById("form-timestamp").value = new Date().toISOString();
